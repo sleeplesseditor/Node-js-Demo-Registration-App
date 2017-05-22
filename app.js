c5f460a6c876486b097fb99ce7af7dd2e53ca378
sleeplesseditor <josh@bryson.me.uk>
2017-05-22 03:56:21 +0100

    Node.js Demo Registration App
    A basic Node.js demo registration app made using features/modules
    including Express.js (3.x), Jade Template Engine, Stylus CSS Generator,
    Nib, MongoDB and Monk.

---
+++ app.js
@@ -0,0 +1,54 @@
+//Module Dependencies
+var express = require('express');
+var stylus = require('stylus');
+var nib = require('nib');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodeapp');
+
+//Set Port Number
+var portnumber = 3000;
+
+//Initialize Express
+var app = express();
+console.log('Express has been initialized');
+
+function compile(str,path){
+	return stylus(str)
+	.set('filename', path)
+	.use(nib())
+}
+
+//Set Views Folder
+app.set('views',__dirname+'/views');
+
+//Initialize Jade
+app.set('view engine', 'jade');
+console.log('Jade has been initialized');
+
+//Stylus Middleware
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('mykey'));
+app.use(express.session());
+app.use(app.router);
+app.use(stylus.middleware(
+	{
+		src:__dirname + '/public',
+		compile: compile
+	}
+))
+app.use(express.static(__dirname+'/public'));
+
+app.get('/',routes.index);
+app.get('/userlist',routes.userlist(db));
+app.post('/adduser',routes.adduser(db));
+
+app.listen(portnumber);
+console.log('Server is now running on port '+portnumber);
---
+++ routes/index.js
@@ -0,0 +1,48 @@
+//Homepage
+exports.index = function(req,res){
+    res.render('index',{title: 'Welcome!'});
+};
+
+//User List
+exports.userlist = function(db){
+    return function(req,res){
+        var collection = db.get('usercollection');
+        collection.find({},{},function(e,docs){
+            res.render('userlist',{
+                "userlist" : docs,
+                title : "User List"
+            });
+        });
+    };
+};
+
+//Add User Form
+exports.adduser = function(db){
+    return function(req,res){
+        //Get Input Submissions
+        var first_name = req.body.first_name;
+        var last_name = req.body.last_name;
+        var username = req.body.username;
+        var email = req.body.email;
+        var password = req.body.password;
+        
+        //Set Collection
+        var collection = db.get('usercollection');
+        
+        //Submit to the DB
+        collection.insert({
+            "first_name" : first_name,
+            "last_name" : last_name,
+            "username" : username,
+            "email" : email,
+            "password" : password
+        },function(err,doc){
+            if(err){
+                res.send('There was a problem');
+            } else {
+                res.location('userlist');
+                res.redirect('userlist');
+            }
+        });
+    }
+}
